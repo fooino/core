e3b455880d45363ad23cbd766bfe1a8ffb0d0693
fooino <sajadsholi@outlook.com>
2025-07-13 17:30:47 +0330

    add-enumable

---
+++ tests/Unit/EnumableUnitTest.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace Fooino\Core\Tests\Unit;
+
+use Fooino\Core\Tests\TestCase;
+use Fooino\Core\Traits\Enumable;
+
+class EnumableUnitTest extends TestCase
+{
+    public function test_values()
+    {
+        $this->assertTrue(Foobar::values() == ['ACTIVE', 'INACTIVE']);
+    }
+
+    public function test_random_value()
+    {
+        $random = Foobar::randomValue();
+        $this->assertTrue($random == 'ACTIVE' || $random == 'INACTIVE');
+    }
+
+    public function test_info_method()
+    {
+        $this->assertTrue(
+            Foobar::info() == [
+                Foobar::maker(key: 'ACTIVE'),
+                Foobar::maker(key: 'INACTIVE')
+            ]
+        );
+    }
+
+    public function test_detail_method()
+    {
+        $this->assertTrue(Foobar::from(value: 'ACTIVE')->detail() == Foobar::maker(key: 'ACTIVE'));
+        $this->assertTrue(Foobar::from(value: 'INACTIVE')->detail() == Foobar::maker(key: 'INACTIVE'));
+    }
+
+    public function test_maker()
+    {
+        $this->assertTrue(
+            Foobar::maker()
+                ==
+                [
+                    'key'       => 'defaultKey',
+                    'name'      => 'unknown',
+                    'mui_icon'  => '',
+                    'color'     => ''
+                ]
+        );
+
+        $this->assertTrue(
+            Foobar::maker(
+                key: Foobar::ACTIVE->value,
+                query: 'status=' . Foobar::ACTIVE->value,
+                endpoint: "languages/1/activate",
+                mui_icon: 'icon',
+                color: 'red',
+                additional: [
+                    'foo'   => 'bar'
+                ]
+            )
+                ==
+                [
+                    'key'       => 'ACTIVE',
+                    'name'      => __('msg.active'),
+                    'endpoint'  => 'languages/1/activate',
+                    'query'     => 'status=ACTIVE',
+                    'mui_icon'  => 'icon',
+                    'color'     => 'red',
+                    'foo'       => 'bar'
+                ]
+        );
+    }
+}
+
+enum Foobar: string
+{
+    use Enumable;
+
+    case ACTIVE   = 'ACTIVE';
+    case INACTIVE = 'INACTIVE';
+
+    public static function active()
+    {
+        return self::maker(
+            key: self::ACTIVE->value
+        );
+    }
+    public static function inactive()
+    {
+        return self::maker(
+            key: self::INACTIVE->value
+        );
+    }
+}
+
+enum Foobar2: string
+{
+    use Enumable;
+
+    case ACTIVE   = 'ACTIVE';
+    case INACTIVE = 'INACTIVE';
+
+    public static function namespaceKey(): string
+    {
+        return 'foobar';
+    }
+}
